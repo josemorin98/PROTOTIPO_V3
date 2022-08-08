8c41ad4f3c45a47500f68e80637a9c3275872a0a
josemorin98 <josemorin9842@gmail.com>
2022-08-08 13:54:43 -0500

    v2

--- pre/Test_correlation.py
+++ pre/Test_correlation.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import os
 import  algo_clustering as clus
 from sklearn import metrics
+import numpy as np
 
 def corr_plot(corr,nameSource,xlabel,ylabel,fol,algo="pearson"):
     if (not os.path.exists("./{}/{}".format(fol,algo))):
@@ -20,10 +21,11 @@ def corr_plot(corr,nameSource,xlabel,ylabel,fol,algo="pearson"):
     plt.close()
     
 def corr(result, columns, algo="pearson"):
+    print("----- {}".format(algo))
     corrs = result.corr(method=algo)
     corrs = corrs[columns]
     corrs = corrs.drop(columns, axis=0)
-    corrs.to_csv("./{}/CORR/fusion_{}.csv".format(fol,anios[x]))
+    corrs.to_csv("./{}/CORR/fusion_{}_{}.csv".format(fol,anios[x],algo))
     return corrs
     
     
@@ -35,15 +37,15 @@ columnsSuic = list(df1.columns)
 
 # print(columnsSuic)
 
-columnsSuciCorr = ['suicAhogamiento', 'suicAhorcamiento', 'suicArma_fuego', 'suicEnvenenamiento', 'suicOtro', 
-                   'suicderhabNE', 'suicSinDerhab', 'suicDerehab', 'suic10_14', 'suic15_19', 'suic20_24', 'suic25_29', 
-                   'suic30_34', 'suic35_39', 'suic40_44', 'suic45_49', 'suic50_54', 'suic55_59', 'suic5_9', 
-                   'suic60_64', 'suicNE_NA', 'pob_00_04', 'pob_05_09', 'pob_10_14', 'pob_15_19', 'pob_20_24', 
-                   'pob_25_29', 'pob_30_34', 'pob_35_39', 'pob_40_44', 'pob_45_49', 'pob_50_54', 'pob_55_59', 
-                   'pob_60_64', 'pob_65_mm', 'tasa_suic5_9', 'tasa_suic10_14', 'tasa_suic15_19', 
-                   'tasa_suic20_24', 'tasa_suic25_29', 'tasa_suic30_34', 'tasa_suic35_39', 'tasa_suic40_44', 
-                   'tasa_suic45_49', 'tasa_suic50_54', 'tasa_suic55_59', 'tasa_suic60_64', 'suic65_mas', 'tasa_suic65_mas',
-                   'tot_pob', 'total_suic', 'tasa_suic']
+# columnsSuciCorr = ['suicAhogamiento', 'suicAhorcamiento', 'suicArma_fuego', 'suicEnvenenamiento', 'suicOtro', 
+#                    'suicderhabNE', 'suicSinDerhab', 'suicDerehab', 'suic10_14', 'suic15_19', 'suic20_24', 'suic25_29', 
+#                    'suic30_34', 'suic35_39', 'suic40_44', 'suic45_49', 'suic50_54', 'suic55_59', 'suic5_9', 
+#                    'suic60_64', 'suicNE_NA', 'pob_00_04', 'pob_05_09', 'pob_10_14', 'pob_15_19', 'pob_20_24', 
+#                    'pob_25_29', 'pob_30_34', 'pob_35_39', 'pob_40_44', 'pob_45_49', 'pob_50_54', 'pob_55_59', 
+#                    'pob_60_64', 'pob_65_mm', 'tasa_suic5_9', 'tasa_suic10_14', 'tasa_suic15_19', 
+#                    'tasa_suic20_24', 'tasa_suic25_29', 'tasa_suic30_34', 'tasa_suic35_39', 'tasa_suic40_44', 
+#                    'tasa_suic45_49', 'tasa_suic50_54', 'tasa_suic55_59', 'tasa_suic60_64', 'suic65_mas', 'tasa_suic65_mas',
+#                    'tot_pob', 'total_suic', 'tasa_suic']
 
 
 anios = [2000,2005,2010,2015,2020]
@@ -57,42 +59,61 @@ for x in range(len(anios)):
         os.mkdir("./{}/CORR".format(fol))
         
     ecnomi_aux = ecnomi[ecnomi["anio"]==anios[x]]
+    
     if (x ==0):
         df1_aux = df1[df1["anio"]<=anios[x]]
     else:
-        df1_aux = df1[df1["anio"]>anios[x-1]] 
-        df1_aux = df1[df1["anio"]<=anios[x]]
-        
+        df1_aux = df1[df1["anio"]>anios[x-1]]
+        df1_aux = df1_aux[df1_aux["anio"]<=anios[x]]
+    
     result = df1_aux.merge(ecnomi_aux, how="inner", left_on=["cve_ent_mun"], right_on=["cve_ent_mun"])
     result.to_csv("./{}/CSV/fusion_{}.csv".format(fol,anios[x]), index=False)
     
-    print("------ {}".format(df1_aux.shape))
-    print("------ {}".format(ecnomi_aux.shape))
-    print("------ {}".format(result.shape))
+    # print("------ {}".format(df1_aux.shape))
+    # print("------ {}".format(ecnomi_aux.shape))
+    # print("------ {}".format(result.shape))
     resultColumns = set(result.columns)
     
     # ----------------------------- Correlacion
     
     result = result.dropna(1)
-    corrs = corr(result=result,
-                 columns=columnsSuciCorr)
-    corr_plot(corrs,"Suic_{}".format(anios[x]),"Variables Suicidios", "Variables Macroeconomicas",fol)
-    corrs_spearman = corr(result=result,
-                          algo="spearman",
-                          columns=columnsSuciCorr)
-    corr_plot(corrs,"Suic_{}".format(anios[x]),"Variables Suicidios", "Variables Macroeconomicas",fol,"spearman")
-    corrs_spearman = corr(result=result,
-                          algo="kendall",
-                          columns=columnsSuciCorr)
-    corr_plot(corrs,"Suic_{}".format(anios[x]),"Variables Suicidios", "Variables Macroeconomicas",fol,"kendall")
+    # corrs = corr(result=result,
+    #              columns=columnsSuciCorr)
+    # corr_plot(corrs,"Suic_{}".format(anios[x]),"Variables Suicidios", "Variables Macroeconomicas",fol)
+    # corrs_spearman = corr(result=result,
+    #                       algo="spearman",
+    #                       columns=columnsSuciCorr)
+    # corr_plot(corrs,"Suic_{}".format(anios[x]),"Variables Suicidios", "Variables Macroeconomicas",fol,"spearman")
+    # corrs_spearman = corr(result=result,
+    #                       algo="kendall",
+    #                       columns=columnsSuciCorr)
+    # corr_plot(corrs,"Suic_{}".format(anios[x]),"Variables Suicidios", "Variables Macroeconomicas",fol,"kendall")
 
     # ----------------------------- Clustering
 
+    # print(result.columns)
+    
     # Kmeans
-    colClus = []
+    df_numerics = result.select_dtypes(include=np.number)
+    colClus = df_numerics.columns
     scoreSil = list()
-    for x in range(3,12):
+    sourceClus = "./{}/K_means".format(fol)
+    if (not os.path.exists(sourceClus)):
+        os.mkdir(sourceClus)
+    if (not os.path.exists("{}/{}".format(sourceClus,anios[x]))):
+        os.mkdir("{}/{}".format(sourceClus,anios[x]))
+    
+    for k in range(3,12):       
         dataClus = result[colClus]
-        kmeanLabel = clus.K_means(k=x,data=dataClus,nameSource="Kmeans_K{}".format(x))
-        scoreSil.append('K{}'.format(x),metrics.silhouette_score(dataClus, kmeanLabel, metric="euclidean"))
+        kmeanLabel = clus.K_means(k=k,data=dataClus,nameSource="Kmeans_K{}".format(k))
+        columname="Kmeans_K{}".format(k)
+        result[columname] = kmeanLabel
+        print(result.columns)
+        clus.mapingMX(df=result,name=columname,columnColor=columname.format(k),k=k)
+        # sil = metrics.silhouette_score(dataClus, kmeanLabel)
+        scoreSil.append(('K{}'.format(k),sil))
+    
+    print(clus.plotingSilhouete(scoreSil,
+                               "K_means",
+                               "{}/{}".format(sourceClus,anios[x])))    
         
--- pre/algo_clustering.py
+++ pre/algo_clustering.py
@@ -4,10 +4,11 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
-import time
-import time
 import matplotlib.pyplot as plt
 import os
+import geopandas as gpd
+import geopandas
+
 
 def read_CSV(name):
     return pd.read_csv(name)
@@ -30,17 +31,15 @@ def K_means(k,data,nameSource):
     except Exception:
         return np.zeros(data.shape[0])
 
-def plotingSilhouete(scoreSil,algo,sourcePath,loggerInfo,loggerError,nodeId, arrivalTime, exitTimeManager,src):
+def plotingSilhouete(scoreSil,algo,sourcePath):
     try:
-        startTime = time.time()
-        arrivalTime = time.time()
-        exitTime = time.time()
+        
         scoreSil.sort(key=lambda x: x[1], reverse=True) # sort 
 
         cluster = list(zip(*scoreSil))[0] # labels
         score = list(zip(*scoreSil))[1] #scores
-        timesSil = list(zip(*scoreSil))[2]
-        mediaTimesSil = np.mean(timesSil)
+        # timesSil = list(zip(*scoreSil))[2]
+        # mediaTimesSil = np.mean(timesSil)
         x_pos = np.arange(len(cluster)) #positions
 
         plt.bar(x_pos, score, align='center', )
@@ -54,26 +53,17 @@ def plotingSilhouete(scoreSil,algo,sourcePath,loggerInfo,loggerError,nodeId, arr
         plt.title('Silhouette Scores {}'.format(algo))
         plt.xlabel('Clustering {}'.format(algo))
 
-
-        nameSource = '{}_silhouette_score_{}_{}'.format(nodeId,algo,src)
-
+        nameSource = 'silhouette_score_{}.png'.format(algo)
         
-        if (not os.path.exists(".{}/{}/Silhouette_Scores".format(sourcePath,nodeId))):
-                os.mkdir(".{}/{}/Silhouette_Scores".format(sourcePath,nodeId))
+        if (not os.path.exists("{}/Silhouette_Scores".format(sourcePath))):
+                os.mkdir("{}/Silhouette_Scores".format(sourcePath))
         # pathSave = ".{}/{}.png".format(sourcePath,nameSource)
-        pathSave = ".{}/{}/Silhouette_Scores/{}".format(sourcePath,nodeId,nameSource)
-        loggerError.error(pathSave)
+        pathSave = "./{}/Silhouette_Scores/{}".format(sourcePath,nameSource)
         plt.savefig(pathSave)
         plt.cla()
-        endTime = time.time()
-        serviceTime = (endTime-startTime)
-        latenceTime = arrivalTime-exitTimeManager
-        exitTime = time.time()
-        loggerInfo.info('SILHOUETTE_DONE {} {} {} {} {}'.format(algo.upper(),(serviceTime+mediaTimesSil),arrivalTime, exitTime, latenceTime))
         return 'OK'
-    except Exception:
-        loggerError.error("SILHOUETTE_FAILED {}".format(algo))
-        return 'NO OK'
+    except Exception as e:
+        return "NO OK \n {}".format(e) 
     
 
 def MixtureModel(k,data,nameSource):
@@ -87,3 +77,35 @@ def MixtureModel(k,data,nameSource):
         return labels
     except Exception:
         return np.zeros(data.shape[0])
+    
+
+def mapingMX(df,name,columnColor,k):
+    mexicoDF = gpd.read_file("./states/Mexico_Estados.shp")
+    mexicoDF["ESTADO"]=mexicoDF["ESTADO"].replace("Distrito Federal","Ciudad de Mexico")
+    columnPolygon="ESTADO"
+    fig, ax = plt.subplots(figsize=(7,5))
+    ax.set_aspect('equal')
+
+    ploting = mexicoDF.plot(ax=ax, color="white",edgecolor="black",linewidth=1.5)
+
+
+    gdf = geopandas.GeoDataFrame(
+        df, geometry=geopandas.points_from_xy(df.lon, df.lat))
+
+    ploting = gdf.plot(ax=ploting,column=columnColor,
+                       categorical=True,
+                       markersize=8,
+                        marker='*',
+                        legend=True,
+                        legend_kwds={'loc': 'center left',
+                                     'bbox_to_anchor':(1,0.5)})
+    
+    # nombre del archivo
+    # ploting.set_axis_off()
+    nameFile = "./results/{}_{}.png".format(name,columnColor)
+    # colocamos el titulo del pngs
+    ploting.set_title('{}\n{}'.format("Clustering",columnColor))
+    ploting.figure.savefig(nameFile)
+    plt.cla()
+    plt.clf()
+    plt.close()
